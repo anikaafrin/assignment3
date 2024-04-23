474648dace0548d95d7686d1931e9a71d8f73265
Anika <anikaafrin520@gmail.com>
2024-04-23 23:44:56 +0600

    initialize

---
+++ main.js
@@ -0,0 +1,15 @@
+// imports 
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.get('/', (req,res) => {
+    res.send("Hello World");
+
+});
+app.listen(PORT, ()=>{
+    console.log(`Server started at http://localhost:${PORT}`);
+});
