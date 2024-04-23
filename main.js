78c0c40615e1d00ccaf8ddfdb7ad683bb02bef54
Anika <anikaafrin520@gmail.com>
2024-04-24 04:17:35 +0600

    do it

--- main.js
+++ main.js
@@ -6,10 +6,38 @@ const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.get('/', (req,res) => {
-    res.send("Hello World");
+//database connection 
+mongoose.connect(process.env.DB_URI,{useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', (error)=> console.log(error));
+db.once('open', ()=> console.log('connected to the database!'));
 
+//middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(session({
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false,
+
+}));
+app.use((req, res, next)=>{
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
 });
+
+
+//set the template engine
+app.set('view engine', 'ejs');
+
+
+
+
+// route prefix
+app.use("", require('./routes/routes'));
+
+
 app.listen(PORT, ()=>{
     console.log(`Server started at http://localhost:${PORT}`);
 });
